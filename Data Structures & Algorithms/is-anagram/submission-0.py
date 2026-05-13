b94752241c543d6fe3323154b6291c213a7e5a6f
Tom Willetts <tom_94_oasis@hotmail.com>
2026-05-13 20:35:44 +0100

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+        
+        s1 = "racecar"
+        s2 = "carrace"
+        sol = Solution()
+        print(sol.isAnagram(s1, s2)) # Output: True
