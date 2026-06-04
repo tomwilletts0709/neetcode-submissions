8cb66225d94ce93ce916823e54be44bc7b1a0780
Tom Willetts <tom_94_oasis@hotmail.com>
2026-06-04 09:26:04 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+        s1 = "racecar"
+        s2 = "carrace"
+        sol = solution()
+        return (sol.isAnagram(s1, s2))
